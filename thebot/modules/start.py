d47f7d7189103854121748327f1a5c87e073fec7
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-04-16 13:41:30 +0700

    Update start.py

--- thebot/modules/start.py
+++ thebot/modules/start.py
@@ -16,7 +16,6 @@ async def start(_, message):
                      [
                         InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
                         InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
-                        InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
                      ],
                      [
                         InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
@@ -25,12 +24,11 @@ async def start(_, message):
                   ]
           await message.reply_text('Available cmds for now :\n /animeinfo - search anime on AniList\n /mangainfo - search manga on Anilist\n /charinfo - search character on Anilist\n /airinfo - check airing status of an anime\n /wa by replying to a media - find what anime a media is from\n /nhentai ID - returns the nhentai in telegraph instant preview format.', reply_markup=InlineKeyboardMarkup(buttons))
      else:
-       photo = "https://telegra.ph/file/19dad86d7b1009f1d6911.jpg"
+       photo = "https://telegra.ph/file/1d8bdb0653a521fa8aa6f.jpg"
        buttons = [
             [
             InlineKeyboardButton('Anime', switch_inline_query_current_chat='anime '),
             InlineKeyboardButton('Manga', switch_inline_query_current_chat='manga '),
-            InlineKeyboardButton('nHentai', switch_inline_query_current_chat='nhentai ')
             ],
             [
             InlineKeyboardButton('Airing', switch_inline_query_current_chat='airing '),
@@ -44,4 +42,4 @@ async def start(_, message):
          caption='Hi, uwu >//////<\nCheck Help to know what I can do ;)\nSeach in Inline by Clicking these buttons below!',
          reply_markup=InlineKeyboardMarkup(buttons))
    else:
-       await message.reply_text("Hi there, I'm EsseX.")
+       await message.reply_text("Hi there, I'm Nekozu!.")
